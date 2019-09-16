493fbb96e9ee86fe7a4b47da1a264265d8db7bc6
rutuja92 <rutujasalway123@gmail.com>
2019-09-16 16:05:16 +0530

    First commit

---
+++ src/com/model/AutoItDemo.java
@@ -0,0 +1,22 @@
+package com.model;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoItDemo 
+{
+public static void main(String args[]) throws IOException
+{
+	System.setProperty("webdriver.chrome.driver","C:\\29062019\\chromedriver_win32\\chromedriver.exe ");
+    WebDriver driver=new ChromeDriver();
+driver.get("file:///E:/autoitfile1.html");
+driver.manage().window().maximize();
+driver.findElement(By.xpath("//input[@type='file']")).click();
+Runtime.getRuntime().exec("E:\\Autoitfileselenium.exe");
+
+}}
+
+
